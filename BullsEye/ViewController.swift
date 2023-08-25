fafce057d3e404399107b5576425dd57441cc014
Danil Anikin <mart4r.clearlist@gmail.com>
2023-08-25 16:09:33 +1000

    Added Button and Alert Message

--- BullsEye/ViewController.swift
+++ BullsEye/ViewController.swift
@@ -2,7 +2,7 @@
 //  ViewController.swift
 //  BullsEye
 //
-//  Created by Данил Аникин on 25.08.2023.
+//  Created by anikin02 on 25.08.2023.
 //
 
 import UIKit
@@ -13,7 +13,20 @@ class ViewController: UIViewController {
     super.viewDidLoad()
     // Do any additional setup after loading the view.
   }
-
-
+  
+  @IBAction func showAlert() {
+    let alert = UIAlertController(
+      title: "Hello world!",
+      message: "This is my first App",
+      preferredStyle: .alert)
+    
+    let action = UIAlertAction(
+      title: "Awesome",
+      style: .default,
+      handler: nil)
+    
+    alert.addAction(action)
+    present(alert, animated: true, completion: nil)
+  }
 }
 
